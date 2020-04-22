62f82514d96b7d6f73ac87ec40bd398c12318d1a
Hazel H Pham <phd.h99@gmail.com>
2020-04-22 18:31:58 +0700

    Create sumOfTwoInt.java

---
+++ JAVA/sumOfTwoInt.java
@@ -0,0 +1,15 @@
+//Author : Hazel Pham
+//Description: Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+//Source: https://leetcode.com/problems/sum-of-two-integers/
+
+/*
+Use method sum() of class Integer to solve. 
+*/ 
+
+class Solution {
+    public int getSum(int a, int b) {
+        int result = 0;
+        result = Integer.sum(a,b);
+        return result; 
+    }
+}
